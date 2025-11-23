1135ab62684aa25ac13aea117a32956210cf31b6
novaacainn23-ship-it <novaacainn23@gmail.com>
2025-11-22 23:03:32 -0500

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -20,8 +20,6 @@ const allowedOrigins = [
   "http://localhost:5173", // or 3000 — whatever you use locally
 ];
 
-import cors from "cors";
-
 const allowedOrigins = [
   "https://final-project-mtg-web-app.vercel.app",
   "http://localhost:5173",
